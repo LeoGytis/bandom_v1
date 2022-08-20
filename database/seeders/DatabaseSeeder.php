e33bcf163a0db89ce688e52c9720d3cffcc6b12c
LeoGytis <leogytis@gmail.com>
2022-08-20 19:23:04 +0300

    The beginning of testing

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace Database\Seeders;
+
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Faker\Factory as Faker;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $faker = Faker::create();
+
+        // // ========================== Country ==========================
+        // foreach (range(1, 10) as $_) {
+        //     $s_date = $faker->dateTimeBetween('-30 days', '+240 days');
+        //     DB::table('countries')->insert([
+        //         'name' => $faker->country,
+        //         's_time' => date_format($s_date, 'Y/m/d'),
+        //     ]);
+        // }
+    
+        // // ========================== Hotel ==========================
+        // foreach (range(1, 10) as $_) {
+        //     $hotels = ['Radisson Blue', 'Clarion', 'Grand Budapest', 'Old Town Hotel', 'Central Hotel', 'Scandic Hotel', 'Park Hotel', 'Grand Tower Hotel', 'Parken Inn'];
+    
+        //     $photopath = 'http://localhost/bandom/public/images/hotels/';
+    
+        //     DB::table('hotels')->insert([
+        //         'name' => $hotels[rand(0, count($hotels) - 1)],
+        //         'price' => rand(100, 500),
+        //         'photo' => $photopath . rand(1,9) . '.jpg',
+        //         'trip_time' => rand(7, 14),
+        //         'country_id' => rand(1, 10),
+        //     ]);
+        // }
+    
+        // ========================== USERS ==========================
+          DB::table('users')->insert([
+            'name' => 'user',
+            'email' => 'user@gmail.com',
+            'password' => Hash::make('123'),
+            'role' => 1,
+        ]);
+    
+        DB::table('users')->insert([
+            'name' => 'admin',
+            'email' => 'admin@gmail.com',
+            'password' => Hash::make('123'),
+            'role' => 10,
+        ]);
+    
+        DB::table('users')->insert([
+            'name' => 'Gytis',
+            'email' => 'leogytis@gmail.com',
+            'password' => Hash::make('123'),
+            'role' => 10,
+        ]);
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,59 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CountryController as Country;
+use App\Http\Controllers\HotelController as Hotel;
+use App\Http\Controllers\OrderController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+// ========================== Country ==========================
+Route::group(['prefix' => 'countries'], function () {
+    Route::get('', [Country::class, 'index'])->name('country.index')->middleware('rp:user');
+    Route::get('create', [Country::class, 'create'])->name('country.create')->middleware('rp:admin');
+    Route::post('store', [Country::class, 'store'])->name('country.store')->middleware('rp:admin');
+    Route::get('edit/{country}', [Country::class, 'edit'])->name('country.edit')->middleware('rp:admin');
+    Route::put('update/{country}', [Country::class, 'update'])->name('country.update')->middleware('rp:admin');
+    Route::post('delete/{country}', [Country::class, 'destroy'])->name('country.destroy')->middleware('rp:admin');
+    Route::get('show/{country}', [Country::class, 'show'])->name('country.show')->middleware('rp:user');
+});
+
+
+// ========================== Hotel ==========================
+Route::prefix('hotels')->controller(Hotel::class)->group(function () {
+    Route::get('', 'index')->name('hotel.index')->middleware('rp:user');
+    Route::get('create', 'create')->name('hotel.create')->middleware('rp:admin');
+    Route::post('store', 'store')->name('hotel.store')->middleware('rp:admin');
+    Route::get('edit/{hotel}', 'edit')->name('hotel.edit')->middleware('rp:admin');
+    Route::put('update/{hotel}', 'update')->name('hotel.update')->middleware('rp:admin');
+    Route::post('delete/{hotel}', 'destroy')->name('hotel.destroy')->middleware('rp:admin');
+    Route::get('show/{hotel}', 'show')->name('hotel.show')->middleware('rp:user');
+    Route::put('delete-picture/{hotel}', 'deletePicture')->name('hotels.delete-picture')->middleware('rp:admin');
+});
+
+
+// ========================== Order ==========================
+Route::prefix('orders')->controller(OrderController::class)->name('order.')->group(function () {
+    Route::get('', 'index')->name('index')->middleware('rp:admin');
+    Route::post('add', 'add')->name('add');
+    Route::post('delete/{order}', 'destroy')->name('destroy')->middleware('rp:admin');
+    Route::put('status/{order}', 'setStatus')->name('status')->middleware('rp:admin');
+    Route::get('show', 'showMyOrders')->name('show');
+});
